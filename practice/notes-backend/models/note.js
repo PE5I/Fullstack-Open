c1e0bc067c3e3d982b5e2a11b0b337b7a98e4b5f
PE5I <116784810+PE5I@users.noreply.github.com>
2022-12-22 01:39:44 -0800

    changed project structure

--- practice/notes-backend/models/note.js
+++ practice/notes-backend/models/note.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url);
-
-mongoose.set('strictQuery', true);
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
--- practice/notes-backend/mongo.js
+++ practice/notes-backend/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>');
+  console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
 
@@ -29,17 +29,17 @@ mongoose
     // })
 
     // return note.save()
-  // })
-  // .then(() => {
+    // })
+    // .then(() => {
     // console.log('note saved!');
-    
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
+
+    Note.find({}).then(result => {
+      result.forEach(note => {
+        console.log(note)
+      })
+      return mongoose.connection.close()
     })
-    return mongoose.connection.close()
-  })
-    
+
     // return mongoose.connection.close()
   })
   .catch((err) => console.log(err))
