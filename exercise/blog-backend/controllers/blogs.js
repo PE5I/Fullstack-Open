7db64410b083a2f2d686e6589d9e17076f456481
PE5I <116784810+PE5I@users.noreply.github.com>
2022-12-31 10:32:46 -0800

    completed exercises 4.15 - 4.19

--- exercise/blog-backend/controllers/blogs.js
+++ exercise/blog-backend/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -7,11 +8,25 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    console.log('substring: ', authorization.substring(7))
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!(decodedToken.id)) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  // const user = await User.findById(body.userId)
 
-  const user = await User.findById(body.userId)
-  // console.log(user)
   const blog = new Blog({
     title: body.title,
     author: body.author,
