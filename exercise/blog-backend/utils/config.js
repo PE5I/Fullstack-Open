5d72d62d851610c13dc3b1c9a851e69dd9028016
PE5I <116784810+PE5I@users.noreply.github.com>
2022-12-31 09:14:12 -0800

    add users to app

--- exercise/blog-backend/utils/config.js
+++ exercise/blog-backend/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const PORT = process.env.PORT || '3003'
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
-  : 'mongodb://localhost/27017'
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
