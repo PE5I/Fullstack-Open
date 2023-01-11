8a981b7fada2ce2de0a705a43e7dbd1619e8e246
PE5I <116784810+PE5I@users.noreply.github.com>
2023-01-11 12:08:27 -0800

    counter app in redux

---
+++ course_projects/redux_counter/src/App.js
@@ -0,0 +1,45 @@
+import { createStore } from 'redux'
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+store.subscribe(renderApp)
+
+export default App;
