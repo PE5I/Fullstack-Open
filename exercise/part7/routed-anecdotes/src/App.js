3f8b87430d1e689afd9be271680432626d6ae066
PE5I <116784810+PE5I@users.noreply.github.com>
2023-01-25 05:07:48 -0800

    completed exercise 7.2

--- exercise/part7/routed-anecdotes/src/App.js
+++ exercise/part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link, useParams
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -12,8 +12,21 @@ const Menu = () => {
     <div>
       {/* <a href='#' style={padding}>anecdotes</a> */}
       <Link to="/" style={padding}>anecdotes</Link>
-      <a href='/create' style={padding}>create new</a>
-      <a href='/about' style={padding}>about</a>
+      <Link to="/create" style={padding}>create new</Link>
+      <Link to="/about" style={padding}>about</Link>
+    </div>
+  )
+}
+
+const Anecdote = ({ anecdoteById }) => {
+  const { id } = useParams()
+  const anecdote = anecdoteById(Number(id))
+
+  return (
+    <div>
+      <h1>{anecdote.content}</h1>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
     </div>
   )
 }
@@ -22,7 +35,11 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{
+        <Link to={`/anecdotes/${anecdote.id}`}>
+          {anecdote.content}
+        </Link>
+      }</li>)}
     </ul>
   </div>
 )
@@ -134,12 +151,9 @@ const App = () => {
         <Menu />
         <Routes>
           <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
-        </Routes>
-        <Routes>
           <Route path="/about" element={<About />} />
-        </Routes>
-        <Routes>
           <Route path="/create" element={<CreateNew addNew={addNew} />} />
+          <Route path="/anecdotes/:id" element={<Anecdote anecdoteById={anecdoteById} />} />
         </Routes>
         <Footer />
       </div>
