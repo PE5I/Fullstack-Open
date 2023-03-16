b497ae48b42fe60c3f02876ff1ae7b9763b00f6c
PE5I <116784810+PE5I@users.noreply.github.com>
2023-03-16 02:46:06 -0700

    refactored bmiCalculator to accept commandline arguments

--- exercise/part9/fitness/bmiCalculator.ts
+++ exercise/part9/fitness/bmiCalculator.ts
@@ -1,3 +1,18 @@
+interface BmiValues {
+  height: number;
+  mass: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values are not numbers')
+  }
+}
 
 const calculateBmi = (height: number, mass: number): string => {
   const bmi = mass / ((height/100) * (height/100))
@@ -6,9 +21,20 @@ const calculateBmi = (height: number, mass: number): string => {
     return "Underweight (severe thinness)"
   } else if (18.5 < bmi && bmi < 25) {
     return "Normal (healthy weight)"
+  } else if (25 < bmi && bmi < 29) {
+    return "Overweight"
+  } else if (bmi >= 30) {
+    return "Obese"
   } else {
     return "unknown"
   }
 }
 
-console.log(calculateBmi(180, 74));
+
+
+try {
+  const { height, mass } = parseArguments(process.argv)
+  console.log(calculateBmi(height, mass))
+} catch (e) {
+  console.log(`Error: ${e}`)
+}
