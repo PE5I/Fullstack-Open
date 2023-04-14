b82f2aef7043f43ee5976835193d3423a0980a56
PE5I <116784810+PE5I@users.noreply.github.com>
2023-04-14 01:34:33 -0700

    completec a

--- exercise/part10/first-app/App.js
+++ exercise/part10/first-app/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("hello");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
