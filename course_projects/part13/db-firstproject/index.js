44def342fc962a62bfc7d7b7b8942833cc6b4b8e
PE5I <116784810+PE5I@users.noreply.github.com>
2023-04-16 03:06:08 -0700

    use sequelize orm

--- course_projects/part13/db-firstproject/index.js
+++ course_projects/part13/db-firstproject/index.js
@@ -1,16 +1,66 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.log('Unable to connect to the database:', error)
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
 
-main()
+app.use(express.json())
+
+app.get('/api/notes', async (req, res) => {
+  // const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    console.log(req.body)
+    const note = await Note.create(req.body)
+    return res.json(note)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
+// const main = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+//     console.log(notes);
+//     sequelize.close()
+//   } catch (error) {
+//     console.log('Unable to connect to the database:', error)
+//   }
+// }
+// main()
