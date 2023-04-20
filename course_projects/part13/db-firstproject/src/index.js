13f1a0d3e08a4cf5a7f8c917a66fc6c0360c21ab
PE5I <116784810+PE5I@users.noreply.github.com>
2023-04-20 02:26:05 -0700

    implement user table

--- course_projects/part13/db-firstproject/src/index.js
+++ course_projects/part13/db-firstproject/src/index.js
@@ -2,12 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const notesRouter = require('./routes/notes')
+const usersRouter = require('./routes/users')
+const loginRouter = require('./routes/login')
 const { connectToDatabase } = require('./util/db')
 const { PORT } = require('./util/config')
 
 app.use(express.json())
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
